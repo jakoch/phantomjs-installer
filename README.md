69ef3e0b36b81982f7c4846fed19c580028cdd06
jakoch <jakoch@web.de>
2013-08-03 13:19:10 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
 phantomjs-installer
 ===================
 
-A Composer Package which installs the PhantomJS binary (Linux, Windows, Mac) into the /bin of your project.
+A Composer package which installs the PhantomJS binary (Linux, Windows, Mac) into `/bin` of your project.
+
+## Installation
+
+To install PhantomJS as a local, per-project dependency to your project, simply add a dependency on `jakoch/phantomjs-installer` to your project's `composer.json` file.
+
+```json
+{
+    "require": {
+        "jakoch/phantomjs-installer": "1.9.1"
+    }
+}
+```
+
+Currently Composer does not pass events to the handler scripts of dependencies.
+You might execute the installer a) manually or b) by adding the following additional settings to your `composer.json`:
+
+```json
+{
+    "scripts": {
+        "post-install-cmd": [
+            "PhantomInstaller\\Installer::installPhantomJS"
+        ],
+        "post-update-cmd": [
+            "PhantomInstaller\\Installer::installPhantomJS"
+        ]
+    }
+}
+```
+
+Now, assuming that the scripts section is set up as required, PhantomJS binaries
+will be installed into the `/bin` folder and updated alongside the project's Composer dependencies.
---
+++ src/PhantomInstaller/Installer.php
@@ -0,0 +1,129 @@
+<?php
+namespace PhantomInstaller;
+
+use Composer\Script\Event;
+
+use Composer\Package\Package;
+use Composer\Package\Version\VersionParser;
+
+class Installer
+{
+    const PHANTOMJS_NAME = 'PhantomJS';
+
+    /**
+     * Operating system dependend installation of PhantomJS
+     */
+    public static function installPhantomJS(Event $event)
+    {
+        $composer = $event->getComposer();
+        $package = $composer->getPackage();
+       
+        # get global "required" packges array to find the "phantomjs-installer" and fetch it's "version"
+        $requiredPackagesArray = $package->getRequires();
+        $phantomjsInstaller_PackageLink = $requiredPackagesArray['jakoch/phantomjs-installer'];
+        $version = $phantomjsInstaller_PackageLink->getPrettyConstraint();
+
+        # fallback to a harccoded version number, if "dev-master" was set
+        if($version === 'dev-master') {
+            $version = '1.9.1';
+        }
+
+        #$io = $event->getIO();
+        #$io->write('<info>Fetching PhantomJS v'.$version.'</info>');
+
+        $name = self::PHANTOMJS_NAME;
+        $url = self::getURL($version);
+
+        # Creating Composer In-Memory Package
+
+        $targetDir = './bin';
+
+        $versionParser = new VersionParser();
+        $normVersion = $versionParser->normalize($version);
+        $package = new Package($name, $normVersion, $version);
+
+        $package->setTargetDir($targetDir);
+        $package->setInstallationSource('dist');
+        $package->setDistType(pathinfo($url, PATHINFO_EXTENSION)); // set zip, tarball
+        $package->setDistUrl($url);
+
+        # Downloading the Archive
+
+        $downloadManager = $event->getComposer()->getDownloadManager();
+        $downloadManager->download($package, $targetDir, false);
+    }
+
+    /**
+     * Returns the URL of the PhantomJS distribution for the installing OS.
+     */
+    public static function getURL($version)
+    {
+        $url = false;
+
+        if (self::getOS() === 'windows') {
+            $url = 'https://phantomjs.googlecode.com/files/phantomjs-' . $version . '-windows.zip';
+        }
+
+        if (self::getOS() === 'linux') {
+            if (self::getBitSize() === 32) {
+                $url = 'https://phantomjs.googlecode.com/files/phantomjs-' .  $version . '-linux-i686.tar.bz2';
+            }
+
+            if (self::getBitSize() === 64) {
+                $url = 'https://phantomjs.googlecode.com/files/phantomjs-' .  $version . '-linux-x86_64.tar.bz2';
+            }
+        }
+
+        if (self::getOS() === 'macosx') {
+            $url = 'https://phantomjs.googlecode.com/files/phantomjs-' .  $version . '--macosx.zip';
+        }
+
+        # OS unknown
+        if($url === false) {
+            throw new \RuntimeException(
+                'The Installer could not select a PhantomJS package for this OS.
+                Please install PhantomJS manually into the /bin folder of your project.'
+            );
+        }
+
+        return $url;
+    }
+
+    /**
+     * Returns the Operating System.
+     *
+     * @return string OS, e.g. macosx, windows, linux.
+     */
+    public static function getOS()
+    {
+        $uname = strtolower(php_uname());
+
+        if (strpos($uname, "darwin") !== false) {
+            return 'maxosx';
+        } elseif (strpos($uname, "win") !== false) {
+            return 'windows';
+        } elseif (strpos($uname, "linux") !== false) {
+            return 'linux';
+        } else {
+            return 'unknown';
+        }
+    }
+
+    /**
+     * Returns the Bit-Size.
+     *
+     * @return string BitSize, e.g. 32, 64.
+     */
+    public static function getBitSize()
+    {
+        if (PHP_INT_SIZE === 4) {
+            return '32';
+        }
+
+        if (PHP_INT_SIZE === 8) {
+            return '64';
+        }
+
+        return PHP_INT_SIZE; // 16-bit?
+    }
+}
